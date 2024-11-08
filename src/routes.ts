d339f69b73af24292433b36b58d12d982c4f100f
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-11-07 22:00:15 -0300

    routing for nginx fixed

--- src/routes.ts
+++ src/routes.ts
@@ -4,54 +4,54 @@ export const Routes = [
 // stat
 {
     method: "get",
-    route: "/api/v1/stats",
+    route: "/stats",
     controller: MainController,
     action: "statsAll"
 }, {
     method: "get",
-    route: "/api/v1/stats/:id",
+    route: "/stats/:id",
     controller: MainController,
     action: "statsOne"
 }, {
     method: "post",
-    route: "/api/v1/stats",
+    route: "/stats",
     controller: MainController,
     action: "statsSave"
 }, {
     method: "patch",
-    route: "/api/v1/stats/:id",
+    route: "/stats/:id",
     controller: MainController,
     action: "statsUpdate"
 }, {
     method: "delete",
-    route: "/api/v1/stats/:id",
+    route: "/stats/:id",
     controller: MainController,
     action: "statsRemove"
 }, 
 //user has stat
 {
     method: "get",
-    route: "/api/v1/users-stats",
+    route: "/users-stats",
     controller: MainController,
     action: "userHasStatAll"
 },{
     method: "get",
-    route: "/api/v1/users-stats/:id",
+    route: "/users-stats/:id",
     controller: MainController,
     action: "userHasStatOne"
 },{
     method: "post",
-    route: "/api/v1/users-stats",
+    route: "/users-stats",
     controller: MainController,
     action: "userHasStatCreate"
 },{
     method: "patch",
-    route: "/api/v1/users-stats/:id",
+    route: "/users-stats/:id",
     controller: MainController,
     action: "userHasStatUpdate"
 },{
     method: "delete",
-    route: "/api/v1/users-stats/:id",
+    route: "/users-stats/:id",
     controller: MainController,
     action: "userHasStatRemoveById"
 },]
